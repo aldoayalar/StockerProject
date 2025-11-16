8771e3219e658220816ed2b96ab0b4d0bc876d6b
Aldo <aldoayalar@hotmail.com>
2025-11-16 08:27:06 -0300

    Agregando Templates del proyecto anterior

---
+++ Core/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    # Url de Vista del login
+    path('', views.login, name='login'),
+
+    # Urls de Vistas basadas en roles
+    path('tecnico/', views.tecnico, name='tecnico'),
+    path('bodega/', views.bodega, name='bodega'),
+    path('chofer/', views.chofer, name='chofer'),
+    path('gerente/', views.gerente, name='gerente'),
+
+    # Urls de Vistas basadas en funcionalidades
+    path('historial_tecnico/', views.historial_tecnico, name='historial_tecnico'),
+    path('solicitud/', views.solicitud, name='solicitud')
+]
--- Core/views.py
+++ Core/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+#----------------------------------------------------------------------------------------
+# Vistas según roles
+
+def tecnico(request):
+    return render(request, 'rol/tecnico.html')
+
+def bodega(request):
+    return render(request, 'rol/bodega.html')
+
+def chofer(request):
+    return render(request, 'rol/chofer.html')
+
+def gerente(request):
+    return render(request, 'rol/gerente.html')
+#----------------------------------------------------------------------------------------
+# Vista Login
+
+def login(request):
+    return render(request, 'general/login.html')
+
+#----------------------------------------------------------------------------------------
+# Vista según funcionalidad
+
+def historial_tecnico(request):
+    return render(request, 'funcionalidad/historial_tecnico.html')
+
+def solicitud(request):
+    return render(request, 'funcionalidad/solicitud.html')
