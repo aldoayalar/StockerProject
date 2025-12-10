b6adf3a5d87923268b52e02d4c41d475516256dc
Aldo <aldoayalar@hotmail.com>
2025-12-09 22:25:04 -0300

    correccion

--- Stocker/settings.py
+++ Stocker/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # URLs de redirección
